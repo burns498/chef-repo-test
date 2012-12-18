baf51b11fa94f0088bb84eb6dfa8578458e68a1a
Andrew <burns498@umn.edu>
2012-12-18 09:26:47 -0600

    rails-app cookbook

---
+++ cookbooks/rails-demo/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "burns498"
+maintainer_email "burns498@umn.edu"
+license          "All rights reserved"
+description      "Installs/Configures rails-demo"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends          "application"
+depends          "application_ruby"
---
+++ cookbooks/rails-demo/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: rails-demo
+# Recipe:: default
+#
+# Copyright 2012, CLAOIT
+#
+# All rights reserved - Do Not Redistribute
+#
+application "rails-demo" do
+  path "/var/www/rails_apps/rails-demo"
+  owner "vagrant"
+  group "vagrant"
+
+  repository "http://github.com/mulderp/chef-demo.git"
+  passenger_apache2
+end
